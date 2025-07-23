9a7570cf15c7f54f9e7df82b4254974a314516b5
Atif Ahmad <40195767+AatifAhmad01@users.noreply.github.com>
2025-07-23 17:51:40 +0430

    Update CORS configuration in app.js to use environment variable for allowed origins, enhancing security and flexibility.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ cloudinary.config({
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.json({ limit: '16kb' }));
 app.use(cookieParser());
